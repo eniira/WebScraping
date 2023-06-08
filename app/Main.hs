669b18f1c27d90f0e26e4cf1babb23fc29e4be91
eniira <catarinecruz@alunos.utfpr.edu.br>
2023-06-07 23:10:59 -0300

    testando ver autor

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ scrapingTravessa livro = do
     let baseUrl = "https://www.travessa.com.br/Busca.aspx?d=1&bt=" ++ livro ++ "&cta=00&codtipoartigoexplosao=&pag="
         page = 1
         firstPageUrl = baseUrl ++ show page
-    result <- scrapeURL firstPageUrl fetch_updates
+    result <- scrapeURL firstPageUrl fetch_links
 
     case result of
         Just updates -> do
@@ -18,31 +18,63 @@ scrapingTravessa livro = do
 
 scrapeNextPagesParallel :: String -> Int -> IO [[String]]
 scrapeNextPagesParallel baseUrl startPage = do
-    let pageNumbers = [startPage..11]  -- Páginas de 2 a 21
-    mapConcurrently_ (scrapeAndPrintPage baseUrl) pageNumbers
-    return []
+    let pageNumbers = [startPage..11]  -- Páginas de 2 a 11
+    mapConcurrently (scrapeAndProcessPage baseUrl) pageNumbers
 
-scrapeAndPrintPage :: String -> Int -> IO ()
-scrapeAndPrintPage baseUrl page = do
+scrapeAndProcessPage :: String -> Int -> IO [String]
+scrapeAndProcessPage baseUrl page = do
     let url = baseUrl ++ show page
-    result <- scrapeURL url fetch_updates
+    result <- scrapeURL url fetch_links
     case result of
-        Just updates -> do
-            putStrLn ("Resultados da página " ++ show page ++ ":")
-            mapM_ putStrLn updates
-            putStrLn ""
-        Nothing -> putStrLn ("Falha ao processar a página " ++ show page)
+        Just links -> do
+            --putStrLn ("Resultados da página " ++ show page ++ ":")
+            processedLinks <- mapM processLink links
+            --putStrLn ""
+            return processedLinks
+        Nothing -> do
+            putStrLn ("Falha ao processar a página " ++ show page)
+            return []
 
-fetch_updates :: Scraper String [String]
-fetch_updates = chroots ("h4" @: [hasClass "search-result-item-heading"]) isolate_update
+fetch_links :: Scraper String [String]
+fetch_links = chroots ("h4" @: [hasClass "search-result-item-heading"]) isolate_link
 
-isolate_update :: Scraper String String
-isolate_update = update
+isolate_link :: Scraper String String
+isolate_link = link_book
 
-update :: Scraper String String
-update = do
-    header <- text "a"
-    return header
+link_book :: Scraper String String
+link_book = do
+    --header <- text $ "a"
+    url <- attr "href" "a"
+    return $ url
+
+processLink :: String -> IO String
+processLink links = do
+    let bookUrl = links
+    --putStrLn $ "Acessando link: " ++ bookUrl
+    result <- scrapeURL bookUrl fetch_book
+    case result of
+        Just bookData -> do
+            --putStrLn "Dados do livro:"
+            mapM_ putStrLn bookData
+            --putStrLn ""
+            return links
+        Nothing -> do
+            putStrLn "Falha ao processar a página do livro"
+            return ""
+
+fetch_book :: Scraper String [String]
+fetch_book = chroots ("div" @: [hasClass "main"]) isolate_book
+
+isolate_book :: Scraper String String
+isolate_book = book
+
+book :: Scraper String String
+book = do
+    title <- text "span"
+    autor <- text "a"
+    --autor <- text $ "span#lblNomArtigo"
+    --autor <- text  "" $ "div" @: [hasClass "autordiretor"] 
+    return autor
 
 main :: IO ()
 main = do
@@ -52,118 +84,7 @@ main = do
     case result of
         Just updates -> do
             putStrLn ("Resultados da página 1:")
-            mapM_ putStrLn updates
+            --mapM_ putStrLn updates
             putStrLn ""
             putStrLn "Processamento concluído"
         Nothing -> putStrLn "Falha ao processar os resultados"
-
-
--- {-# LANGUAGE OverloadedStrings #-}
-
--- import Text.HTML.Scalpel
-
--- scrapingTravessa :: String -> IO (Maybe [String])
--- scrapingTravessa livro = do
---     let baseUrl = "https://www.travessa.com.br/Busca.aspx?d=1&bt=" ++ livro ++ "&cta=00&codtipoartigoexplosao="
---         page = 1
---         firstPageUrl = baseUrl ++ show page
---     result <- scrapeURL firstPageUrl fetch_updates
-
---     case result of
---         Just updates -> do
---             rest <- scrapeNextPages baseUrl (page + 1)
---             return (Just (updates ++ rest))
---         Nothing -> return Nothing
-
--- scrapeNextPages :: String -> Int -> IO [String]
--- scrapeNextPages baseUrl page = do
---     let url = baseUrl ++ show page
---     result <- scrapeURL url fetch_updates
---     case result of
---         Just updates -> do
---             moreUpdates <- scrapeNextPages baseUrl (page + 1)
---             return (updates ++ moreUpdates)
---         Nothing -> return []
-
--- fetch_updates :: Scraper String [String]
--- fetch_updates = chroots ("h4" @: [hasClass "search-result-item-heading"]) isolate_update
-        
--- isolate_update :: Scraper String String
--- isolate_update = update
-        
--- update :: Scraper String String
--- update = do 
---     header <- text "a"
---     return header
-
--- main :: IO ()
--- main = do
---     putStrLn "Digite o livro que deseja encontrar:"
---     livro <- getLine
---     result <- scrapingTravessa livro
---     case result of
---         Just updates -> mapM_ putStrLn updates
---         Nothing -> putStrLn "Failed to scrape updates"
-
-
-
-
-
-
-
--- {-# LANGUAGE OverloadedStrings #-}
-
--- import Text.HTML.Scalpel
--- import Control.Monad
--- -- import Text.HTML.Scalpel.Core
--- -- import Control.Applicative
-
-
--- scrapingTravessa :: String -> IO (Maybe [String])
--- scrapingTravessa livro = do
---     let baseUrl = "https://www.travessa.com.br/Busca.aspx?d=1&bt=" ++ livro ++ "&cta=00&codtipoartigoexplosao=1&pag=2"
---     -- let page = 2
---     -- let firstPageUrl = baseUrl ++ show page
---     -- result <- scrapeURL firstPageUrl fetch_updates
---     scrapeURL baseUrl fetch_updates
---     -- case result of
---     --     Just updates -> do
---     --         rest <- scrapeNextPages baseUrl (page + 1)
---     --         return (updates ++ rest)
---     --     Nothing -> return []
-
---     -- scrapeNextPages :: String -> Int -> IO [String]
---     -- scrapeNextPages baseUrl page = do
---     --     let url = baseUrl ++ show page
---     --     result <- scrapeURL url fetch_updates
---     --     case result of
---     --         Just updates -> do
---     --             moreUpdates <- scrapeNextPages baseUrl (page + 1)
---     --             return (updates ++ moreUpdates)
---     --         Nothing -> return []
---     where
---     fetch_updates :: Scraper String [String]
---     fetch_updates = chroots ("h4" @: [hasClass "search-result-item-heading"]) isolate_update
-        
---     isolate_update :: Scraper String String
---     isolate_update = update
-        
---     update :: Scraper String String
---     update = do 
---        header <- text  $ "a"
---        return $ header
-
--- main :: IO ()
--- main = do
---     putStrLn "Digite o livro que deseja encontrar:"
---     livro <- getLine
---     result <- scrapingTravessa livro
---     case result of
---         Just updates -> mapM_ putStrLn updates
---         Nothing -> putStrLn "Failed to scrape updates"
-
-        
-
-        
-                   
-
